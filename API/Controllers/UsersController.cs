eff83633e1c0d31970450f8de08e9413815ac5f2
hamzamara <123108807+hamzamara@users.noreply.github.com>
2023-07-24 20:15:12 +0200

    Added Controllers and Entities

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users =  await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if ( user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
